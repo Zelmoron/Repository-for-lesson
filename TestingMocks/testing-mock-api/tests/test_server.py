195fc08198866aadfaa9a32191161d755fd136e6
Zelmoron <ak.igor13@mail.ru>
2025-02-18 00:50:40 +0800

    Create comments

--- TestingMocks/testing-mock-api/tests/test_server.py
+++ TestingMocks/testing-mock-api/tests/test_server.py
@@ -12,27 +12,32 @@ def client():
         yield client
 
 def test_registration(client):
+    """Проверка регистрации"""
     response = client.post("/registration", json={"username": "John", "password": "password123"})
     assert response.status_code == 200
     assert response.json()["message"] == "registration successful"
 
 def test_get_users(client):
+    """Проверка получения всех пользователей"""
     response = client.get("/get-users")
     assert response.status_code == 200
-    assert isinstance(response.json(), list)  # Убедитесь, что возвращается список
+    assert isinstance(response.json(), list)  
 
 def test_upload_user_data(client):
+    """Проверка обновлениея"""
     csv_data = "Password,City,Age,Hobby\n123,NewCity,30,Football"
     response = client.get("/upload/Tom", params={"data": csv_data})
     assert response.status_code == 200
     assert response.json()["message"]["City"] == "NewCity"
 
 def test_get_information(client):
+    """Проверка получения инфы об отдельном пользователе"""
     response = client.get("/get_information/Tom")
     assert response.status_code == 200
-    assert response.json()["message"]["City"] == "NewCity"  # Проверяем, что данные обновились
+    assert response.json()["message"]["City"] == "NewCity" 
 
 def test_registration_duplicate(client):
+    """Проверка на дубликат"""
     response = client.post("/registration", json={"username": "Tom", "password": "password123"})
     assert response.status_code == 200
     assert response.json()["message"] == "this user already exists"
